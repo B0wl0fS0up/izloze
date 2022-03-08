1d150a3377d12d3de3c29643a2cad5d74f08ad11
OsamaBinladder <es1188mr@gmail.com>
2022-03-08 10:18:17 +0200

    pirmais
    fixed garbage code

---
+++ script.js
@@ -0,0 +1,5 @@
+const vardi=[`Artūrs`,`Jānis`,`Mārtiņš`,`Ričards`,`Al-habīds`];
+const balvas=[`19$ Fortnite dāvanu karte`,`Juan`,`Turcijas impērija`,`vieta CIA watchlistā`,`šie rieksti`]
+ const naudaKopa=0.03
+ let uzvsk=5
+ console.log (vardi[rand]);
