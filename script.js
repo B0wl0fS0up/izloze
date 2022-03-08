576768c5d9b62f16a53dce254dbf4ca919b9d4b3
OsamaBinladder <es1188mr@gmail.com>
2022-03-08 11:14:30 +0200

    new
    new

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ const vardi=[`Artūrs`,`Jānis`,`Mārtiņš`,`Ričards`,`Al-habīds`];
 const balvas=[`19$ Fortnite dāvanu karte`,`Juan`,`Turcijas impērija`,`vieta CIA watchlistā`,`šie rieksti`]
  const naudaKopa=0.03
  let uzvsk=5
- console.log (vardi[rand]);
+ let rindas = document.querySelector('rindas');
+
+ for(let i=0;i<uzvsk;i++){
+let rand=Math.random()*vardi.length;
+rand=Math.floor(rand);
+
+ console.log (vardi[rand]);}
+ rindas.innerHTML +=``
+ <tr>$
